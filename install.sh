7b6ca79f565424280bef9d0c8c458788bacbd9ac
Andrea C. Martinez <acmthinks@users.noreply.github.com>
2016-03-09 09:29:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,20 +7,18 @@ git clone http://git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.python.git
 cd org.eclipse.paho.mqtt.python
 sudo python setup.py install
 
-# Setting sensor data generation script as executable
-cd ~
-cp ~/connectThingMQTT/connect.py ~
-chmod 755 connect.py
-
-# (Optional) Install Python library for controlling GPIO ports
-sudo pip install RPi.GPIO
+# Setting scripts as executable
+chmod 755 ~/connectThingMQTT/*.py
 
 # Let's validate that we can get a temp reading and light up the LED
 echo ""
 echo "============== Sending test message to iot.eclipse.org:1883 =============="
 echo ""
-sudo python ~/connect.py
+sudo python ~/connectThingMQTT/publish.py
 echo ""
 echo "=============================================================================="
-
-
+echo "To subscribe to the MQTT broker, in a seperate ssh window, run: "
+echo "    sudo python ~/connectThingMQTT/subscribe.py"
+echo ""
+echo "Then in seperate ssh window run: "
+echo "    sudo python ~/connectThingMQTT/publish.py"
