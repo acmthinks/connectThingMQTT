da3c80a96684ac9664e083824109f994c6dacaa2
Andrea C. Martinez <acmthinks@users.noreply.github.com>
2016-03-09 09:23:50 -0500

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -28,6 +28,7 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, rc):
   print("Connected with result code " + str(rc))
+  client.subscribe("connectThingMQTT")
 
 def on_message(client, userdata, msg):
   print ("Topic: " + msg.topic + "\nMessage: " + str(msg.payload))
@@ -38,6 +39,4 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect("iot.eclipse.org", 1883, 60)
-client.subscribe("connectThingMQTT", 0)
-client.unsubscribe("connectThingMQTT")
-client.disconnect()
+client.loop_forever()
